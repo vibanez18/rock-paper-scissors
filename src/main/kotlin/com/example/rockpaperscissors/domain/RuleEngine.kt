4fe0deb5e545cd8830e8ef709940cb2fa35eff8d
victor <vibanez18@gmail.com>
2022-12-02 08:45:11 +0200

    add logger info

--- src/main/kotlin/com/example/rockpaperscissors/domain/RuleEngine.kt
+++ src/main/kotlin/com/example/rockpaperscissors/domain/RuleEngine.kt
@@ -3,16 +3,22 @@ package com.example.rockpaperscissors.domain
 import com.example.rockpaperscissors.domain.Move.ROCK
 import com.example.rockpaperscissors.domain.Move.PAPER
 import com.example.rockpaperscissors.domain.Move.SCISSORS
+import com.example.rockpaperscissors.domain.Score.DRAW
+import com.example.rockpaperscissors.domain.Score.COMPUTER_WIN
+import com.example.rockpaperscissors.domain.Score.PLAYER_ONE_WIN
+import org.apache.logging.log4j.kotlin.Logging
 
-class RuleEngine {
+class RuleEngine: Logging {
     operator fun invoke(computer: Player, playerOne: Player): Score {
         return if(computer.move == playerOne.move) {
-            return Score.DRAW
+            logger.info("Computer moves: ${computer.move} and playerOne: ${playerOne.move}. Score was Draw")
+            return DRAW
         } else {
+            logger.info("No Draw. Computer moves: ${computer.move} and playerOne: ${playerOne.move}.")
             when(computer.move) {
-                ROCK -> if (playerOne.move == SCISSORS) return Score.COMPUTER_WIN else Score.PLAYER_ONE_WIN
-                PAPER -> if (playerOne.move == ROCK) return Score.COMPUTER_WIN else Score.PLAYER_ONE_WIN
-                SCISSORS -> if (playerOne.move == PAPER) return Score.COMPUTER_WIN else Score.PLAYER_ONE_WIN
+                ROCK -> if (playerOne.move == SCISSORS) return COMPUTER_WIN else PLAYER_ONE_WIN
+                PAPER -> if (playerOne.move == ROCK) return COMPUTER_WIN else PLAYER_ONE_WIN
+                SCISSORS -> if (playerOne.move == PAPER) return COMPUTER_WIN else PLAYER_ONE_WIN
             }
         }
     }

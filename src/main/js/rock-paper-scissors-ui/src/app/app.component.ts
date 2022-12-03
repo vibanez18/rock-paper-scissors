1edd7aa4ec7a7f053558dbbf14c3a9bd8e5ad043
victor <vibanez18@gmail.com>
2022-12-03 16:29:02 +0200

    first version app

--- src/main/js/rock-paper-scissors-ui/src/app/app.component.ts
+++ src/main/js/rock-paper-scissors-ui/src/app/app.component.ts
@@ -5,6 +5,57 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'rock-paper-scissors-ui';
+
+  scores = {
+    computer: 0,
+    person: 0
+  };
+
+  computer: any = false;
+  person: any = false;
+
+  choices = ['rock', 'paper', 'scissors'];
+
+  random() {
+    return Math.floor(Math.random() * this.choices.length);
+  }
+
+  play(answer: string) {
+    const computer = this.choices[this.random() | 0];
+    const match = computer + '_' + answer;
+    console.log('computer', computer);
+    switch(match) {
+      case 'rock_paper':
+      case 'paper_scissors':
+      case 'scissors_rock':
+        console.log('Computer loses!');
+        console.log('You win!');
+        this.scores.person++;
+        break;
+      case 'paper_rock':
+      case 'scissors_paper':
+      case 'rock_scissors':
+        console.log('Computer wins!');
+        console.log('You lose!');
+        this.scores.computer++;
+        break;
+      default:
+        console.log('Draw!');
+        this.scores.computer++;
+        this.scores.person++;
+    }
+
+    this.computer = computer;
+    this.person = answer;
+
+  }
+
+  reset() {
+    this.computer = false;
+    this.person = false;
+    this.scores.computer = 0;
+    this.scores.person = 0;
+  }
 }

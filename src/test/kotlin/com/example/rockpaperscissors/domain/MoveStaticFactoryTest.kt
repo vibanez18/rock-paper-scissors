149f7ab0b8f3a313e548837d56c859639040b42a
victor <vibanez18@gmail.com>
2022-12-30 15:18:14 +0100

    add MoveStaticFactory tests

--- src/test/kotlin/com/example/rockpaperscissors/domain/MoveStaticFactoryTest.kt
+++ src/test/kotlin/com/example/rockpaperscissors/domain/MoveStaticFactoryTest.kt
@@ -1,16 +1,33 @@
 package com.example.rockpaperscissors.domain
 
+import com.example.rockpaperscissors.domain.model.Move
+import com.example.rockpaperscissors.domain.model.MoveStaticFactory
+import com.example.rockpaperscissors.domain.model.MoveType
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 class MoveStaticFactoryTest {
 
     @Test
     fun `when withRandomMove then return random Move`() {
+        val randomMove = MoveStaticFactory.withRandomMove()
 
+        assertThat(randomMove)
+            .isNotNull
+            .isInstanceOf(Move::class.java)
     }
 
-    @Test
-    fun `when withMoveType then return Move by name`() {
+    @ParameterizedTest
+    @EnumSource(value = MoveType::class, names = ["ROCK", "PAPER", "SCISSORS"])
+    fun `when withMoveType then return Move by name`(moveType: MoveType) {
+        val moveWithMoveType = MoveStaticFactory.withMoveType(moveType)
 
+        assertThat(moveWithMoveType)
+            .isInstanceOf(Move::class.java)
+
+        assertThat(moveWithMoveType.nameMove)
+            .isEqualTo(moveType)
     }
 }

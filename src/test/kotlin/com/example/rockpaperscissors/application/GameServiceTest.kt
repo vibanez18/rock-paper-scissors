f9741eedd4d76d52990d6a825cf54d1ce4ef299f
victor <vibanez18@gmail.com>
2022-12-30 15:32:47 +0100

    add GameService tests

--- src/test/kotlin/com/example/rockpaperscissors/application/GameServiceTest.kt
+++ src/test/kotlin/com/example/rockpaperscissors/application/GameServiceTest.kt
@@ -1,8 +1,47 @@
 package com.example.rockpaperscissors.application
 
-class GameServiceTest {
-    //TODO: param test
-    fun `when createAndExecuteGame then return gameDto`() {
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 
+@ExtendWith(MockitoExtension::class)
+class GameServiceTest {
+
+    @InjectMocks
+    lateinit var gameService: GameService
+
+    @Test
+    fun `when createAndExecuteGame with valid movement then return gameDto`() {
+
+        val gameDtoMock = mock<GameDto> {
+            on { playerOneName } doReturn "playerOne"
+            on { playerOneMove } doReturn "ROCK"
+        }
+
+        val (playerOneName, playerOneMove, computerName, computerMove, score) = gameService.createAndExecuteGame(gameDtoMock)
+
+        assertThat(playerOneName).isEqualTo("playerOne")
+        assertThat(playerOneMove).isEqualTo("ROCK")
+        assertThat(computerName).isEqualTo("Computer")
+        assertThat(computerMove).isNotEmpty
+        assertThat(score).isNotNull
+    }
+
+    @Test
+    fun `when createAndExecuteGame with invalid movement then thrown exception`() {
+
+        val gameDtoMock = mock<GameDto> {
+            on { playerOneName } doReturn "playerOne"
+            on { playerOneMove } doReturn "FAIL"
+        }
+
+        assertThatThrownBy { gameService.createAndExecuteGame(gameDtoMock) }
+            .isInstanceOf(IllegalArgumentException::class.java)
+            .hasMessage("No enum constant com.example.rockpaperscissors.domain.model.MoveType.FAIL")
     }
 }
